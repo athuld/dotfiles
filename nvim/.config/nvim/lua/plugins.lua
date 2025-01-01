0e026e72719b02b2b01a6e69fc3fd246c904d423
Athul Dinesan <athuldinesan5@gmail.com>
2025-01-01 21:01:06 +0530

    nvim-cmp updates and go.nvim
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -93,6 +93,9 @@ return {
     event = {'BufReadPre', 'BufNewFile'},
     dependencies = {
       {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-cmdline'},
       {'onsails/lspkind.nvim'},
       {'williamboman/mason.nvim'},
       {'williamboman/mason-lspconfig.nvim'},
@@ -103,6 +106,20 @@ return {
     config = function() require 'configs.lsp-installer' end
   }
 },
+    {
+      "ray-x/go.nvim",
+      dependencies = {  -- optional packages
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+        require("go").setup()
+      end,
+      event = {"CmdlineEnter"},
+      ft = {"go", 'gomod'},
+      build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
     {
         'nvimdev/lspsaga.nvim',
         config = function()
